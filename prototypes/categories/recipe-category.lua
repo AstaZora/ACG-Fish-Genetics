1973bfd35b7211be517f429965f2b8a8e08a5c5e
Asta Zora <Tas_King@outlook.com>
2024-02-26 14:35:19 -0500

    Fish Genetic Modules

---
+++ prototypes/categories/recipe-category.lua
@@ -0,0 +1,22 @@
+data:extend({
+    
+	{
+		type = "item-subgroup",
+		name = "acg-genetics",
+		group = "acg-fish-genetics",
+		order = "f",
+	},
+	{
+		type = "item-group",
+		name = "acg-fish-genetics",
+		icon = "__base__/graphics/technology/advanced-material-processing.png",
+		icon_size = 256,
+		icon_mipmaps = 4,
+		inventory_order = "j",
+		order = "i",
+	},
+    {
+        type = "recipe-category",
+        name = "genetics",
+    },
+})
