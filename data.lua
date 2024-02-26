8f4c9a83afd0279c49d3a5222441a0c83c438f46
Asta Zora <Tas_King@outlook.com>
2024-02-26 16:14:55 -0500

    First step towards fish genetics mod

--- data.lua
+++ data.lua
@@ -7,3 +7,49 @@ require("prototypes.technologies.technologies")
 require("prototypes.categories.fuel-category")
 require("prototypes.entity.resources")
 require("prototypes.entity.autoplace-controls")
+
+-- List of fish types
+local fishTypes = {
+    "glowfin-trenchers",
+    "mukmoux",
+    "silverscale-glider"
+}
+
+-- Function to create genetic items for a fish type
+local function createGeneticItemsForFishType(fishType)
+    local geneticItems = {}
+    
+    -- Generic Genetic Sample
+    table.insert(geneticItems, {
+        type = "item",
+        name = fishType .. "-genetic-sample",
+        icon = "__base__/graphics/icons/fish.png", -- Placeholder icon
+        icon_size = 64,
+        subgroup = "acg-genetics",
+        order = "a[" .. fishType .. "-genetic-sample]",
+        stack_size = 100
+    })
+
+    -- Trait items: G, Y, B
+    local traits = {"g", "y", "b"}
+    for _, trait in ipairs(traits) do
+        table.insert(geneticItems, {
+            type = "item",
+            name = fishType .. "-" .. trait .. "-trait",
+            icon = "__base__/graphics/icons/fish.png", -- Placeholder icon
+            icon_size = 64,
+            subgroup = "acg-genetics",
+            order = "b[" .. fishType .. "-" .. trait .. "-trait]",
+            stack_size = 100
+        })
+    end
+
+    return geneticItems
+end
+
+-- Generate and extend data for each fish type
+for _, fishType in ipairs(fishTypes) do
+    local geneticItems = createGeneticItemsForFishType(fishType)
+    data:extend(geneticItems)
+end
+
